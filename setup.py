e7df0d3be9bee33fdf717cec0181d8e6a09aa536
Frans <franssiswanto@gmail.com>
2017-01-18 17:11:28 +0800

    rename package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'tagger',
-  packages = ['tagger'], # this must be the same as the name above
+  name = 'artagger',
+  packages = ['artagger'], # this must be the same as the name above
   version = '0.1',
   description = 'RDRPOSTagger implementation',
   author = 'Frans Huang',
   author_email = 'franssiswanto@gmail.com',
-  url = 'https://github.com/franziz/tagger', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/franziz/artagger', # use the URL to the github repo
+  download_url = 'https://github.com/franziz/artagger/tarball/0.1', # I'll explain this in a second
   keywords = ['rdr', 'pos tagger', 'pos_tags'], # arbitrary keywords
   classifiers = [],
 )
